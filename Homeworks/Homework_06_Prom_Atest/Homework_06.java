e0c24c7b4fe666635c5df4d5a0bbc5a7b4541727
nagovitsin <nagovitsin-kgs@mail.ru>
2023-07-12 13:46:58 +0700

    Lecture_06

---
+++ Homeworks/Homework_06_Prom_Atest/Homework_06.java
@@ -0,0 +1,18 @@
+package Homeworks.Homework_05.Homework_06_Attestation;
+
+import java.util.*;
+
+/**
+ * Задание
+ * Реализуйте структуру телефонной книги с помощью HashMap.
+ * Программа также должна учитывать, что во входной структуре будут
+ * повторяющиеся имена с разными телефонами,
+ * их необходимо считать, как одного человека с разными телефонами.
+ * Вывод должен быть отсортирован по убыванию числа телефонов.
+ */
+
+public class Homework6 {
+    public static void main(String[] args) {
+        System.out.println("Goodbye world");
+    }
+}
